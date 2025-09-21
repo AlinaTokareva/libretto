10d4039204fc402859a321a6a1dd77fa3ce13e73
AlinaTokareva <tokalina97@mail.ru>
2025-09-21 22:38:14 +0800

    Вишлист - верстка

--- app/(tabs)/library.tsx
+++ app/(tabs)/library.tsx
@@ -5,35 +5,13 @@ import {Center} from '@/components/ui/center'
 
 
 const Library = () => {
-    const booksData = [
-        {source: require('@/assets/images/book_covers/Винсент Ван Гог.jpg'), title: '',},
-        {source: require('@/assets/images/book_covers/2.00x-thumb.png'), title: '',},
-        {source: require('@/assets/images/book_covers/theAustereAcademy.jpg'), title: '',},
-        {source: require('@/assets/images/book_covers/430fb8fa-689a-4fba-86ab-a2a446d9cb2f.jpg'), title: '',},
-        {source: require('@/assets/images/book_covers/e47f1f07-f763-4bef-bdd4-bb1cdff613db.jpg'), title: '',},
-        {source: require('@/assets/images/book_covers/unnamed.jpg'), title: '',},
-        {source: require('@/assets/images/book_covers/unnamed (1).jpg'), title: '',},
-    ]
-    const books = booksData.map(item =>
-        <Image
-            source={item.source}
-            style={{
-                height: 170,
-                width: 110,
-                borderRadius: 10,
-                margin: 5,
-            }}
-            key={item.source.toString()}
-        />
-    )
+
 
     return (
         <View>
             <SafeAreaView>
                 <Center className={'w-full flex-row flex-wrap'}>
-                    {/*<VStack style={styles.box}>*/}
-                    {books}
-                    {/*</VStack>*/}
+
                 </Center>
             </SafeAreaView>
         </View>
--- app/(tabs)/wishlist.tsx
+++ app/(tabs)/wishlist.tsx
@@ -1,21 +1,20 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import {View} from '@/components/Themed'
 import {Fab, FabIcon} from '@/components/ui/fab'
 import {AddIcon} from '@/components/ui/icon'
 import * as ImagePicker from 'expo-image-picker'
-import {supabase} from '@/config/supabase'
-import {decode} from 'base64-arraybuffer'
-import {useAuth} from '@/providers/AuthProvider'
 import {Image} from '@/components/ui/image'
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context'
 import {ScrollView} from 'react-native'
-import {FileObject} from '@supabase/storage-js'
 import {Heading} from '@/components/ui/heading'
+import {VStack} from '@/components/ui/vstack'
 
 
 type Wish = {
     id: string,
-    source: { uri: string }
+    source: {
+        uri: string
+    }
 }
 
 type WishRecord = {
@@ -25,88 +24,21 @@ type WishRecord = {
 }
 
 const Profile = () => {
-    const {user,} = useAuth()
-
     const [wishes, setWishes,] = useState<Wish[]>([])
 
-    useEffect(() => {
-        initData()
-    }, [])
-
-    const initData = async () => {
-        const {data, error,} = await supabase
-            .from('Wish')
-            .select()
-
-        if (error) console.error(error)
-        console.log(data)
-
-        data?.forEach(item => loadImage(item))
-    }
-
-
-    const loadImage = async (record: WishRecord) => {
-        supabase.storage
-            .from('Files')
-            .download(`${user!.id}/wishlist/${record.id}`)
-            .then(({data,}) => {
-                const fr = new FileReader()
-                fr.readAsDataURL(data!)
-                fr.onload = () => {
-                    const newWish = {
-                        id: record.id,
-                        source: {
-                            uri: fr.result as string,
-                        },
-                    }
-
-                    setWishes([...wishes, newWish,])
-                }
-            })
-
-
-    }
 
     const pickImage = async () => {
         const result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ['images',],
             allowsEditing: false,
-            base64: true,
+            // base64: true,
             quality: 1,
         })
 
-        if (!result.canceled) {
-            const {data: dataInsert, error: errorInsert,} = await supabase
-                .from('Wish')
-                .insert({user: user!.id,})
-                .select({
-                    id: true,
-                })
-
-            console.log(dataInsert)
-
-            if (errorInsert) {
-                console.error(errorInsert)
-            }
-
-            const img = result.assets[0]
-            const base64 = img.base64 as string
-            const filePath = `${user!.id}/wishlist/${dataInsert?.[0].id}.png`
-            const contentType = 'image/png'
-
-            const {error,} = await supabase.storage.from('Files').upload(
-                filePath,
-                decode(base64),
-                {
-                    contentType,
-                })
-
-            if (error) {
-                console.error(error)
-            }
-
-            loadImage(dataInsert)
-        }
+        setWishes([...wishes, {
+            id: result?.assets?.[0].assetId as string,
+            source: {uri: result?.assets?.[0].uri as string,},
+        },])
     }
 
     return (
@@ -121,16 +53,21 @@ const Profile = () => {
                 </Fab>
 
                 <SafeAreaView className={'p-5'}>
-                    <Heading size={'2xl'}>Вишлист</Heading>
-                    <ScrollView className={'flex-wrap flex-col'}>
-                        {wishes.map(item =>
-                            <Image
-                                key={item.id}
-                                source={item.source}
-                                className={'h-[200px] aspect-cover rounded-md'}
-                                alt={'wish'}
-                            />
-                        )}
+                    <Heading size={'2xl'} className={'mb-3'}>Вишлист</Heading>
+                    <ScrollView>
+                        <VStack
+                            className={'flex-row flex-wrap'}
+                        >
+
+                            {wishes.map(item =>
+                                <Image
+                                    key={item.source.uri}
+                                    source={item.source}
+                                    className={'w-1/3 sm:w-1/4 md:w-1/5 aspect-cover rounded-md p-3'}
+                                    alt={'wish'}
+                                />
+                            )}
+                        </VStack>
                     </ScrollView>
                 </SafeAreaView>
             </View>
