de0fc30f75a5617e43b11b80b42c3ef1cc7b9f63
AlinaTokareva <tokalina97@mail.ru>
2025-09-25 22:31:36 +0800

    Вишлист - небольшие правки

--- app/(tabs)/wishlist.tsx
+++ app/(tabs)/wishlist.tsx
@@ -1,13 +1,18 @@
 import React, {useState} from 'react'
 import {View} from '@/components/Themed'
 import {Fab, FabIcon, FabLabel} from '@/components/ui/fab'
-import {AddIcon} from '@/components/ui/icon'
+import {AddIcon, SearchIcon} from '@/components/ui/icon'
 import * as ImagePicker from 'expo-image-picker'
 import {Image} from '@/components/ui/image'
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context'
 import {Heading} from '@/components/ui/heading'
 import {Grid, GridItem} from '@/components/ui/grid'
 import {ScrollView} from 'react-native'
+import {Input, InputField, InputIcon, InputSlot} from '@/components/ui/input'
+import {Text} from '@/components/ui/text'
+import {Center} from '@/components/ui/center'
+import {HStack} from '@/components/ui/hstack'
+import {Badge, BadgeText} from '@/components/Badge'
 
 
 type Wish = {
@@ -50,32 +55,48 @@ const Wishlist = () => {
                     <FabLabel>Загрузить фото</FabLabel>
                 </Fab>
 
-                <SafeAreaView className={'pr-5 pl-5 pt-5 flex-1'}>
+                <SafeAreaView className={'pr-5 pl-5 pt-5 gap-3 flex-1'}>
+                    <Heading size={'2xl'}>Вишлист</Heading>
 
-                    <Heading size={'2xl'} className={'mb-3'}>Вишлист</Heading>
-                    <ScrollView>
-                        <Grid
-                            className={'gap-3 mb-5'}
-                            _extra={{
-                                className: 'grid-cols-3 sm:grid-cols-4 md:grid-cols-6',
-                            }}
-                        >
-                            {wishes.map(item =>
-                                <GridItem
-                                    key={item.source.uri}
-                                    _extra={{
-                                        className: 'col-span-1',
-                                    }}
-                                >
-                                    <Image
-                                        source={item.source}
-                                        className={'w-full h-auto rounded-lg aspect-cover'}
-                                        alt={'wish'}
-                                    />
-                                </GridItem>
-                            )}
-                        </Grid>
-                    </ScrollView>
+                    <HStack className={'gap-2'}>
+                        <Badge action={'error'}>
+                            <BadgeText>Классика</BadgeText>
+                        </Badge>
+                        <Badge>
+                            <BadgeText>Фантастика</BadgeText>
+                        </Badge>
+                    </HStack>
+
+                    {!wishes.length ? (
+                        <Center className={'m-auto'}>
+                            <Text className={'color-background-500'}>Здесь ничего нет</Text>
+                            <Text className={'color-background-500'}>Нужно только пожелать!</Text>
+                        </Center>
+                    ) : (
+                        <ScrollView>
+                            <Grid
+                                className={'gap-3 mb-5'}
+                                _extra={{
+                                    className: 'grid-cols-3 sm:grid-cols-4 md:grid-cols-6',
+                                }}
+                            >
+                                {wishes.map(item =>
+                                    <GridItem
+                                        key={item.source.uri}
+                                        _extra={{
+                                            className: 'col-span-1',
+                                        }}
+                                    >
+                                        <Image
+                                            source={item.source}
+                                            className={'w-full h-auto rounded-lg aspect-cover'}
+                                            alt={'wish'}
+                                        />
+                                    </GridItem>
+                                )}
+                            </Grid>
+                        </ScrollView>
+                    )}
                 </SafeAreaView>
             </View>
         </SafeAreaProvider>
